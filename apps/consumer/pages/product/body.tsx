bda85addd12160a2406feb88ab6e0d6129fbc319
LoiNguyen94 <loinh@mioapp.vn>
2022-04-07 17:06:51 +0700

    make it better

--- apps/consumer/pages/product/body.tsx
+++ apps/consumer/pages/product/body.tsx
@@ -13,51 +13,52 @@ export function Body({ data }) {
 
   return (
     <Row style={{ marginLeft: 0, marginRight: 0 }} gutter={[16, 16]}>
-      {data.map(
-        (item: any) =>
-          item.photo && (
-            <Col
-              lg={4}
-              sm={6}
-              xs={8}
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}
-              key={item.id}
-              //   span={4}
-            >
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={
-                  <Image
-                    alt="example"
-                    src={item?.photo}
-                    width={240}
-                    height={230}
-                    priority
-                  />
-                }
-                actions={[
-                  <div key="price" className={styles['rainbow']}>
-                    {formatMoneyVND(item?.market_price)}
-                  </div>,
-                  <Image
-                    key="plus"
-                    alt="example"
-                    src={'/pluscart.png'}
-                    width={24}
-                    height={24}
-                    priority
-                  />,
-                ]}
+      {data &&
+        data.map(
+          (item: any) =>
+            item.photo && (
+              <Col
+                lg={4}
+                sm={6}
+                xs={8}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+                key={item.id}
+                //   span={4}
               >
-                <Meta title={item?.name} description={item?.category} />
-              </Card>
-            </Col>
-          )
-      )}
+                <Card
+                  hoverable
+                  style={{ width: 240 }}
+                  cover={
+                    <Image
+                      alt="example"
+                      src={item?.photo}
+                      width={240}
+                      height={230}
+                      priority
+                    />
+                  }
+                  actions={[
+                    <div key="price" className={styles['rainbow']}>
+                      {formatMoneyVND(item?.market_price)}
+                    </div>,
+                    <Image
+                      key="plus"
+                      alt="example"
+                      src={'/pluscart.png'}
+                      width={24}
+                      height={24}
+                      priority
+                    />,
+                  ]}
+                >
+                  <Meta title={item?.name} description={item?.category} />
+                </Card>
+              </Col>
+            )
+        )}
     </Row>
   );
 }
