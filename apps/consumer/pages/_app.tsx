7369f670363abcc4c8d4fd72b63ac15e079ec02c
LoiNguyen94 <loinh@mioapp.vn>
2022-04-12 11:55:07 +0700

    fix conflict

--- apps/consumer/pages/_app.tsx
+++ apps/consumer/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-<<<<<<< HEAD
-=======
 import { Provider } from 'react-redux';
 // import withRedux from 'next-redux-wrapper';
 // import { initializeStore } from './../store/store';\
 import { wrapper } from './../store/store';
->>>>>>> ed703815911de717f00e35ea454c911afedb64dc
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,21 +13,13 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to consumer!</title>
       </Head>
       <main className="app">
-<<<<<<< HEAD
-        <Component {...pageProps} />
-=======
         {/* <Provider store={initializeStore()}> */}
         <Component {...pageProps} />
         {/* </Provider> */}
->>>>>>> ed703815911de717f00e35ea454c911afedb64dc
       </main>
     </>
   );
 }
 
-<<<<<<< HEAD
-export default CustomApp;
-=======
 // export default CustomApp;
 export default wrapper.withRedux(CustomApp);
->>>>>>> ed703815911de717f00e35ea454c911afedb64dc
