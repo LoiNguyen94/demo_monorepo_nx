dad4086a0eb5a8ab796a6605c54cf14b11b835ec
LoiNguyen94 <loinh@mioapp.vn>
2022-04-12 11:49:50 +0700

    merge develop

--- apps/consumer/next.config.js
+++ apps/consumer/next.config.js
@@ -15,11 +15,7 @@ const nextConfig = {
     swcMinify: true,
     experimental: {
       urlImports: ['https://cdn.skypack.dev'],
-<<<<<<< HEAD
-      outputStandalone: true
-=======
       outputStandalone: true,
->>>>>>> ed703815911de717f00e35ea454c911afedb64dc
     },
   },
 };
